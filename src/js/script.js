d802073339f705f8c25a065f173d6767b5e5ea67
Miron Fissenko <mirfees@gmail.com>
2024-05-01 19:02:09 +0500

    Добавил новые блоки, оптимизировал картинки, начал стилизацию

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
         // Your custom settings go here
     });
 
+    //begin sliders
 
+
+
+    const mainSlider = new Swiper('.hero__swiper', {
+        // Optional parameters
+        loop: true,
+        spaceBetween: 20,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+
+    //end sliders
 });
