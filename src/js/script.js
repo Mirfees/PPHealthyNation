2b4d301a6c450ed33d12867bcc920c77e1e152d2
Miron Fissenko <mirfees@gmail.com>
2024-05-21 19:02:36 +0500

    Сделал страницу пробного аккаунта и частично страницу главного аккаунта

--- src/js/script.js
+++ src/js/script.js
@@ -31,9 +31,50 @@ document.addEventListener('DOMContentLoaded', function () {
     //begin BURGER-MENU
     let burgerMenu = document.getElementById('burger-menu');
     let overlay = document.querySelector('.header-menu');
-    burgerMenu.addEventListener('click',function(){
-        this.classList.toggle("close");
-        overlay.classList.toggle("overlay");
-    });
+    if (burgerMenu) {
+        burgerMenu.addEventListener('click',function(){
+            this.classList.toggle("close");
+            overlay.classList.toggle("overlay");
+        });
+    }
     //end BURGER-MENU
+
+    let accountTabs = new Tabs('.account-tabs-container');
 });
+
+class Tabs {
+    constructor(tabsContainerSelector) {
+        this.tabsContainer = document.querySelector(tabsContainerSelector);
+        this.tabTitleClass = 'account-header-menu__link';
+        this.tabClass = 'account-tab';
+
+
+        if (this.tabsContainer) {
+            this.tabsContainer.addEventListener('click', (e) => {
+                let target = e.target;
+
+                if(target.closest(`.${this.tabTitleClass}`)) {
+                    let tabTitle = (target.closest(`.${this.tabTitleClass}`));
+                    let tabTitles = this.tabsContainer.querySelectorAll(`.${this.tabTitleClass}`);
+                    let tabId = tabTitle.dataset.tabId;
+                    let tab = this.tabsContainer.querySelector('#' + tabId);
+                    let tabs = this.tabsContainer.querySelectorAll(`.${this.tabClass}`);
+
+                    tabTitles.forEach(title => {
+                        title.classList.remove('active');
+                    });
+
+                    tabs.forEach(tab => {
+                        tab.classList.remove('active');
+
+                        if (tab.id === tabId) {
+                            tab.classList.add('active');
+                        }
+                    });
+
+                    tabTitle.classList.add('active');
+                }
+            });
+        }
+    }
+}
